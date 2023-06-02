b3c3148148eef19d4472f58d92c44699cdabf960
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-06-02 20:16:14 +0700

    complete landing and search

--- server/middleware/uploadPictureMiddleware.js
+++ server/middleware/uploadPictureMiddleware.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
 const uploadPicture = multer({
   storage: storage,
   limits: {
-    fileSize: 1 * 10000000, // 1MB
+    fileSize: 1 * 10000000, // 1GB
   },
   fileFilter: function (req, file, cb) {
     let ext = path.extname(file.originalname);
